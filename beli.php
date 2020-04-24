77a52957958960e1a2a915d8aef6b83ec9d30d4c
rizkinurlahid <greenrizky354@gmail.com>
2020-04-24 14:31:36 +0700

    change bootstrap

--- beli.php
+++ beli.php
@@ -124,7 +124,7 @@ if (isset($_POST["submit"])) {
 						</div>
 						<div class="form-group">
 							<label for="banyak">Banyak Barang</label>
-							<input type="number" name="banyak" class="form-control" id="banyak" placeholder="Banyak Barang" required="" min="1" autofocus="">
+							<input type="number" name="banyak" class="form-control" id="banyak" placeholder="Banyak Barang" required="" min="1" autofocus="" value="1">
 						</div>
 						<button type="submit" class="btn btn-primary" name="submit">Beli</button>
 					</form>
@@ -133,10 +133,13 @@ if (isset($_POST["submit"])) {
 		</div>
 	</div>
 
-	<script src="bootstrap-4.3.1\dist\js\jquery-3.3.1.slim.min.js"></script>
+	<!-- <script src="bootstrap-4.3.1\dist\js\jquery-3.3.1.slim.min.js"></script>
 	<script src="bootstrap-4.3.1\dist\js\popper.min.js"></script>
 	<script src="bootstrap-4.3.1\dist\js\bootstrap.min.js"></script>
-	<script src="bootstrap-4.3.1\dist\js\jquery.min.js" type="text/javascript"></script>
+	<script src="bootstrap-4.3.1\dist\js\jquery.min.js" type="text/javascript"></script> -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- functions.php
+++ functions.php
@@ -1,110 +1,117 @@
-<?php 
-	//$conn = mysqli_connect("sql310.epizy.com", "epiz_24132456", "FlCWAMYeea","epiz_24132456_penjualan" );
-$conn = mysqli_connect("localhost", "root", "","penjualan" );
+<?php
+//$conn = mysqli_connect("sql310.epizy.com", "epiz_24132456", "FlCWAMYeea","epiz_24132456_penjualan" );
+$conn = mysqli_connect("localhost", "root", "", "penjualan");
 
-	function query($query){
-		global $conn;
-		$result = mysqli_query($conn, $query);
-		$rows = [];
-		while ($row = mysqli_fetch_assoc($result)) {
-		    $rows[] = $row;
-		}
-		return $rows;
+function query($query)
+{
+	global $conn;
+	$result = mysqli_query($conn, $query);
+	$rows = [];
+	while ($row = mysqli_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+	return $rows;
+}
+
+function tambah($data)
+{
+	global $conn;
+	$gambar = upload();
+	if (!$gambar) {
+		return false;
 	}
 
-	function tambah($data){
-		global $conn;
-		$gambar = upload();
-		if(!$gambar){
-			return false;
-		}
+	$nama = htmlspecialchars($data["nama"]);
+	$harga = htmlspecialchars($data["harga"]);
+	$stok = htmlspecialchars($data["stok"]);
+	$jenis = $data["jenis"];
 
-		$nama = htmlspecialchars( $data["nama"]);
-		$harga = htmlspecialchars( $data["harga"]);
-		$stok = htmlspecialchars( $data["stok"]);
-		$jenis = $data["jenis"];
-
-		$query = "INSERT INTO barang
+	$query = "INSERT INTO barang
 					values 
 					('', '$gambar', '$nama', '$harga', '$stok', '$jenis' )";
-		mysqli_query($conn, $query);
-		return mysqli_affected_rows($conn);
-	}
+	mysqli_query($conn, $query);
+	return mysqli_affected_rows($conn);
+}
 
-	function upload(){
-		$namaFile = $_FILES['gambar']['name'];
-		$ukuranFile = $_FILES['gambar']['size'];
-		$error = $_FILES['gambar']['error'];
-		$tmpName = $_FILES['gambar']['tmp_name'];
+function upload()
+{
+	$namaFile = $_FILES['gambar']['name'];
+	$ukuranFile = $_FILES['gambar']['size'];
+	$error = $_FILES['gambar']['error'];
+	$tmpName = $_FILES['gambar']['tmp_name'];
 
-		if($error === 4){
-			echo "<script>
+	if ($error === 4) {
+		echo "<script>
 					alert('Pilih gambar terlebih dahulu!');
 				  </script>";
-			return false;
-		}
+		return false;
+	}
 
-		$ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
-		$ekstensiGambar = explode('.', $namaFile);
-		$ekstensiGambar = strtolower(end($ekstensiGambar));
-		if (!in_array($ekstensiGambar, $ekstensiGambarValid)){
-			echo "<script>
+	$ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+	$ekstensiGambar = explode('.', $namaFile);
+	$ekstensiGambar = strtolower(end($ekstensiGambar));
+	if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+		echo "<script>
 					alert('Bukan gambar!');
 				  </script>";
-			return false;	
-		}
+		return false;
+	}
 
-		if($ukuranFile > 1000000){
-			echo "<script>
+	if ($ukuranFile > 1000000) {
+		echo "<script>
 					alert('Ukuran file terlalu besar!');
 				  </script>";
-			return false;
-		}
-
-		$namaFileBaru = uniqid();
-		$namaFileBaru .= '.';
-		$namaFileBaru .= $ekstensiGambar; 
-
-		move_uploaded_file($tmpName, 'img/'.$namaFileBaru);
-		return $namaFileBaru;
+		return false;
 	}
 
-	function hapus($id){
-		global $conn;
-		mysqli_query($conn, "DELETE FROM barang WHERE id=$id");
-		return mysqli_affected_rows($conn);
+	$namaFileBaru = uniqid();
+	$namaFileBaru .= '.';
+	$namaFileBaru .= $ekstensiGambar;
+
+	move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
+	return $namaFileBaru;
+}
+
+function hapus($id)
+{
+	global $conn;
+	mysqli_query($conn, "DELETE FROM barang WHERE id=$id");
+	return mysqli_affected_rows($conn);
+}
+
+function hapuskeranjang($no)
+{
+	global $conn;
+	mysqli_query($conn, "DELETE FROM keranjang WHERE no=$no");
+	return mysqli_affected_rows($conn);
+}
+
+function hapususer($id)
+{
+	global $conn;
+	mysqli_query($conn, "DELETE FROM login WHERE id=$id");
+	mysqli_query($conn, "DELETE FROM formulir WHERE id=$id");
+	mysqli_query($conn, "DELETE FROM keranjang WHERE id=$id");
+	return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+	global $conn;
+	$id = $data["id"];
+	$nama = htmlspecialchars($data["nama"]);
+	$harga = htmlspecialchars($data["harga"]);
+	$stok = htmlspecialchars($data["stok"]);
+	$gambarLama = htmlspecialchars($data["gambarLama"]);
+	$jenis = $data["jenis"];
+
+	if ($_FILES['gambar']['error'] === 4) {
+		$gambar = $gambarLama;
+	} else {
+		$gambar = upload();
 	}
 
-	function hapuskeranjang($no){
-		global $conn;
-		mysqli_query($conn, "DELETE FROM keranjang WHERE no=$no");
-		return mysqli_affected_rows($conn);
-	}
-
-	function hapususer($id){
-		global $conn;
-		mysqli_query($conn, "DELETE FROM login WHERE id=$id");
-		mysqli_query($conn, "DELETE FROM formulir WHERE id=$id");
-		mysqli_query($conn, "DELETE FROM keranjang WHERE id=$id");
-		return mysqli_affected_rows($conn);
-	}
-
-	function ubah($data){
-		global $conn;
-		$id = $data["id"];
-		$nama = htmlspecialchars( $data["nama"]);
-		$harga = htmlspecialchars( $data["harga"]);
-		$stok = htmlspecialchars( $data["stok"]);
-		$gambarLama = htmlspecialchars( $data["gambarLama"]);
-		$jenis = $data["jenis"];
-
-		if( $_FILES['gambar']['error'] === 4){
-			$gambar = $gambarLama;
-		}else{
-			$gambar = upload();
-		}
-
-		$query = "UPDATE barang SET 
+	$query = "UPDATE barang SET 
 					gambar = '$gambar',
 					nama = '$nama',
 					harga = '$harga',
@@ -112,112 +119,118 @@ $conn = mysqli_connect("localhost", "root", "","penjualan" );
 					jenis = '$jenis' 
 					WHERE id = $id
 					";
-		mysqli_query($conn, $query);
-		return mysqli_affected_rows($conn);
+	mysqli_query($conn, $query);
+	return mysqli_affected_rows($conn);
+}
+
+function ubahkeranjang($data)
+{
+	global $conn;
+	$no = $data["no"];
+	$id = $data["id"];
+	$id1 = $data["id1"];
+	$nama = htmlspecialchars($data["nama"]);
+	$harga = htmlspecialchars($data["harga"]);
+	$banyakLama = $data["banyakLama"];
+	$banyak = $data["banyak"];
+	$stok = $data["stok"];
+	$jenis = $data["jenis"];
+
+
+	$gambar = $data["gambar"];
+
+	if ($banyak > $banyakLama) {
+		$banyakFIX = $banyak;
+		$stokFIX = $stok - ($banyak - $banyakLama);
+	} else if ($banyak < $banyakLama) {
+		$banyakFIX = $banyak;
+		$stokFIX = ($banyakLama - $banyak) + $stok;
 	}
+	$total = $harga * $banyakFIX;
 
-	function ubahkeranjang($data){
-		global $conn;
-		$no = $data["no"];
-		$id = $data["id"];
-		$id1 = $data["id1"];
-		$nama = htmlspecialchars( $data["nama"]);
-		$harga = htmlspecialchars( $data["harga"]);
-		$banyakLama = $data["banyakLama"];
-		$banyak = $data["banyak"];
-		$stok = $data["stok"];
-		$jenis = $data["jenis"];
-		$total = $harga*$banyak;
-
-			$gambar = $data["gambar"];
-		
-		if($banyak>$banyakLama){
-			$banyakFIX = $banyak;
-			$stokFIX = $stok-($banyak-$banyakLama);
-		}else if($banyak<$banyakLama){
-			$banyakFIX = $banyak;
-			$stokFIX = ($banyakLama-$banyak)+$stok;
-		}
-
-		$query = "UPDATE keranjang SET
-					banyak = '$banyakFIX'
+	$query = "UPDATE keranjang SET
+					banyak = '$banyakFIX',
+					total = '$total'
 					WHERE no = $no
 					";
-		mysqli_query($conn, $query);	
-		$query1 = "UPDATE barang SET 
-					stok = '$stokFIX'
+	mysqli_query($conn, $query);
+	$query1 = "UPDATE barang SET 
+					stok = '$stokFIX'					
 					WHERE id = $id1
 					";
-		mysqli_query($conn, $query1);
-		return mysqli_affected_rows($conn);
-	}
+	mysqli_query($conn, $query1);
+	return mysqli_affected_rows($conn);
+}
 
-	function register($data){
-		global $conn;
+function register($data)
+{
+	global $conn;
 
-		$username = strtolower(stripslashes($data["username"])) ;
-		$password = mysqli_real_escape_string($conn, $data["password"]);
-		$passwordd = mysqli_real_escape_string($conn, $data["passwordd"]);
+	$username = strtolower(stripslashes($data["username"]));
+	$password = mysqli_real_escape_string($conn, $data["password"]);
+	$passwordd = mysqli_real_escape_string($conn, $data["passwordd"]);
 
-		$result= mysqli_query($conn, "SELECT username FROM login WHERE username='$username'");
-		if(mysqli_fetch_assoc($result)){
-			echo "<script>
+	$result = mysqli_query($conn, "SELECT username FROM login WHERE username='$username'");
+	if (mysqli_fetch_assoc($result)) {
+		echo "<script>
               alert('Username sudah terdaftar!');
 			</script>";
-			return false;
-		}
+		return false;
+	}
 
-		if($password !== $passwordd){
-			echo "<script>
+	if ($password !== $passwordd) {
+		echo "<script>
               alert('Konfirmasi password tidak sesuai!');
 			</script>";
-			return false;
-		}
-
-		$password = password_hash($password, PASSWORD_DEFAULT);
-
-
-		mysqli_query($conn, "INSERT INTO login VALUES ('', '$username', '$password' , '0')");
-		return mysqli_affected_rows($conn);
+		return false;
 	}
 
-	function form($data){
-		global $conn;
-		$gambar = upload();
-		if(!$gambar){
-			return false;
-		}
-		
-		$id = $data["id"];
-		$nama = htmlspecialchars( $data["nama"]);
-		$noidentitas = htmlspecialchars( $data["noidentitas"]);
-		$email = htmlspecialchars( $data["email"]);
-		$nohp = htmlspecialchars( $data["nohp"]);
-		$jk = htmlspecialchars( $data["jk"]);
-		$query = "INSERT INTO formulir
+	$password = password_hash($password, PASSWORD_DEFAULT);
+
+
+	mysqli_query($conn, "INSERT INTO login VALUES ('', '$username', '$password' , '0')");
+	return mysqli_affected_rows($conn);
+}
+
+function form($data)
+{
+	global $conn;
+	$gambar = upload();
+	if (!$gambar) {
+		return false;
+	}
+
+	$id = $data["id"];
+	$nama = htmlspecialchars($data["nama"]);
+	$noidentitas = htmlspecialchars($data["noidentitas"]);
+	$email = htmlspecialchars($data["email"]);
+	$nohp = htmlspecialchars($data["nohp"]);
+	$jk = htmlspecialchars($data["jk"]);
+	$query = "INSERT INTO formulir
 					VALUES 
 					('$id', '$gambar', '$nama', '$noidentitas', '$email', '$nohp', '$jk' )";
-		mysqli_query($conn, $query);
-		return mysqli_affected_rows($conn);
+	mysqli_query($conn, $query);
+	return mysqli_affected_rows($conn);
+}
+
+function profil($data)
+{
+	global $conn;
+	$id = $data["id"];
+	$nama = htmlspecialchars($data["nama"]);
+	$noidentitas = htmlspecialchars($data["noidentitas"]);
+	$email = htmlspecialchars($data["email"]);
+	$nohp = htmlspecialchars($data["nohp"]);
+	$jk = htmlspecialchars($data["jk"]);
+	$gambarLama = htmlspecialchars($data["gambarLama"]);
+
+	if ($_FILES['gambar']['error'] === 4) {
+		$gambar = $gambarLama;
+	} else {
+		$gambar = upload();
 	}
 
-	function profil($data){
-		global $conn;
-		$id = $data["id"];
-		$nama = htmlspecialchars( $data["nama"]);
-		$noidentitas = htmlspecialchars( $data["noidentitas"]);
-		$email = htmlspecialchars( $data["email"]);
-		$nohp = htmlspecialchars( $data["nohp"]);
-		$jk = htmlspecialchars( $data["jk"]);
-		$gambarLama = htmlspecialchars( $data["gambarLama"]);
-
-		if( $_FILES['gambar']['error'] === 4){
-			$gambar = $gambarLama;
-		}else{
-			$gambar = upload();
-		}
-
-		$query = "UPDATE formulir SET 
+	$query = "UPDATE formulir SET 
 					gambar = '$gambar',
 					nama = '$nama',
 					noidentitas = '$noidentitas',
@@ -226,35 +239,36 @@ $conn = mysqli_connect("localhost", "root", "","penjualan" );
 					jk = '$jk' 
 					WHERE id = $id
 					";
-		mysqli_query($conn, $query);
-		return mysqli_affected_rows($conn);
+	mysqli_query($conn, $query);
+	return mysqli_affected_rows($conn);
+}
+
+function keranjang($data)
+{
+	global $conn;
+
+	$id = $data["id"];
+	$id1 = $data["id1"];
+	$gambar = $data["gambar"];
+	$nama = htmlspecialchars($data["nama"]);
+	$harga = htmlspecialchars($data["harga"]);
+	$banyak = $data["banyak"];
+	$total = $harga * $banyak;
+	$stok = $data["stok"];
+	$jenis = $data["jenis"];
+	if ($banyak < $stok) {
+		$hasil = $stok - $banyak;
+	} else if ($banyak == $stok) {
+		$hasil = 0;
+	} else if ($banyak > $stok) {
+		$hasil = $stok;
+		echo "<script>
+			alert('Banyak barang terlalu besar');
+			</script>";
+		return false;
 	}
 
-	function keranjang($data){
-		global $conn;
-		
-		$id = $data["id"];
-		$id1 = $data["id1"];
-		$gambar = $data["gambar"];
-		$nama = htmlspecialchars( $data["nama"]);
-		$harga = htmlspecialchars( $data["harga"]);
-		$banyak = $data["banyak"];
-		$total = $harga*$banyak;
-		$stok = $data["stok"];
-		$jenis = $data["jenis"];
-		if($banyak < $stok){
-			$hasil = $stok-$banyak;
-		}else if($banyak == $stok){
-			$hasil = 0;
-		}else if($banyak > $stok){
-			$hasil =$stok;
-			echo "<script>
-              alert('Banyak barang terlalu besar');
-			</script>";
-			return false;
-		}
-
-		$query1 = "UPDATE barang SET 
+	$query1 = "UPDATE barang SET 
 					gambar = '$gambar',
 					nama = '$nama',
 					harga = '$harga',
@@ -262,14 +276,14 @@ $conn = mysqli_connect("localhost", "root", "","penjualan" );
 					jenis = '$jenis' 
 					WHERE id = $id1
 					";
-		mysqli_query($conn, $query1);
-		
-		$ker = query("SELECT * FROM keranjang");
-		foreach ($ker as $row) {
-			if($row['no']==$id1){
-				if($row['id']==$id){
-				$banyakk=$row['banyak']+$banyak;
-				$totall = $harga*$banyakk;
+	mysqli_query($conn, $query1);
+
+	$ker = query("SELECT * FROM keranjang");
+	foreach ($ker as $row) {
+		if ($row['no'] == $id1) {
+			if ($row['id'] == $id) {
+				$banyakk = $row['banyak'] + $banyak;
+				$totall = $harga * $banyakk;
 
 
 				$query1 = "UPDATE keranjang SET 
@@ -282,33 +296,42 @@ $conn = mysqli_connect("localhost", "root", "","penjualan" );
 				alert('Pembelian sedang diproses!');
 					document.location.href = 'user.php';
 				</script>";
-				
-			
-				stop();
-			}}		
+
+
+				// stop();
+			}
 		}
-			$query = "INSERT INTO keranjang
+	}
+	$checkIdProduct = query("SELECT * FROM keranjang WHERE no = $id1")[0];
+	if ($checkIdProduct == true) {
+		$query = "UPDATE keranjang SET 
+					banyak = '$banyakk',
+					total = '$totall' 
+					WHERE no = $id1
+					";
+	} else {
+		$query = "INSERT INTO keranjang
 						values 
 						('', '$id1', '$id',  '$gambar', '$nama', '$harga', '$banyak', '$total' )";
-			mysqli_query($conn, $query);
+	}
 
-		
-		return mysqli_affected_rows($conn);
-
+	mysqli_query($conn, $query);
 
 
-	}		
+	return mysqli_affected_rows($conn);
+}
 
-	
+
 
 ?>
 <html>
-	<script type="text/javascript">
-		function Angkasaja(evt) {
+<script type="text/javascript">
+	function Angkasaja(evt) {
 		var charCode = (evt.which) ? evt.which : event.keyCode
 		if (charCode > 31 && (charCode < 48 || charCode > 57))
-		return false;
+			return false;
 		return true;
-		}
-	</script>
+	}
+</script>
+
 </html>
